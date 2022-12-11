9f22650de7589cbb16c9cff7f09b553eb3b2cb52
Scolpe <63779111+Scolpe@users.noreply.github.com>
2022-12-11 01:46:00 +0100

    AoC Day 7

--- Advent_of_Code/2022/Day 7/handler.py
+++ Advent_of_Code/2022/Day 7/handler.py
@@ -1,9 +1,8 @@
 import regex as re
- 
+
 cd_regex = re.compile(r'(?:^\$ cd) (.*.)')
 dir_regex = re.compile(r'dir (.*.)')
 file_regex = re.compile(r'(\d+) (.*.)')
-
 
 class Path_Tree:
     def __init__(self, name, parent):
@@ -13,16 +12,7 @@ class Path_Tree:
         self.files = dict() # {Name of file: Size}
         self.size = 0
     
-    def print_tree(self):
-        current_dir= self
-        for child_name in self.children:
-            current_dir = current_dir.children[child_name]
-            for file in self.files:
-                print(r'-{}, size:{}'.format(file, self.files[file]))
-
-
 root = Path_Tree('root', 'Root')
-
 with open(r'Advent_of_Code\2022\Day 7\source.txt', 'r') as file:
     current_dir = root
     for line in file:
@@ -32,22 +22,44 @@ with open(r'Advent_of_Code\2022\Day 7\source.txt', 'r') as file:
             else:
                 name = cd_regex.search(line)
                 name = str(name.group(1))
-                child = Path_Tree(name, current_dir)
-                current_dir.children[name] = child
-                current_dir = current_dir.children[name]
-        elif line[:3] == 'dir':
-            name = dir_regex.search(line)
-            name = str(name.group(1))
-            child = Path_Tree(name, current_dir)
-            current_dir.children[name] = child
+                try:
+                    current_dir = current_dir.children[name]
+                except:
+                    child = Path_Tree(name, current_dir)
+                    current_dir.children[name] = child
+                    current_dir = current_dir.children[name]
         elif line[:2].isnumeric():
             size_and_name = file_regex.search(line)
             size = int(size_and_name.group(1))
             name = size_and_name.group(2)
             current_dir.size += size
             current_dir.files[name] = size
+            par_dir = current_dir.parent
+            while par_dir.parent != 'Root':
+                par_dir.size += size
+                par_dir = par_dir.parent
         else:
             pass
 
-#print(root.children[r'/'].children[r'jtrbrcjl'].children)
-print(root.children[r'/'].children[r'jtrbrcjl'].size)
+below_10000 = []
+space_needed = 30000000 - 24933642
+
+def call_children(tree):
+    print("{} that is of size {}".format(tree.name, tree.size))
+    if tree.size <= 100000:
+        below_10000.append(tree.size)
+    
+    if tree.children:
+        for child in tree.children:
+            call_children(tree.children[child])
+
+fits = []
+def find_smallets(tree):
+    margin = space_needed - tree.size
+    if margin < 0:
+        fits.append(tree.size)
+        print("New best fit founded: delete {} which will give a margin of {}".format(tree.name, margin))
+    
+    if tree.children:
+        for child in tree.children:
+            find_smallets(tree.children[child])
