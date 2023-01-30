e66b27765c74567fa71ad6682762318e328971d2
Scolpe <63779111+Scolpe@users.noreply.github.com>
2023-01-30 22:16:27 +0100

    Update breadth_first_search.py
    Fixed a bugs occuring during generating a directed graph.

--- Sample_Algo/breadth_first_search.py
+++ Sample_Algo/breadth_first_search.py
@@ -14,26 +14,26 @@ def bfs(graph, searched_element, starting_point=0):
             else:
                 search_queue += graph[element]
                 searched.append(element)
-    print("Element {} is not in the graph or can be reached from the starting point".format(searched_element))
+    print("Element {} is not in the graph or can not be reached from the element {}".\
+        format(searched_element, starting_point))
     return False
 
 def graph_maker(elements_no=10, directed = True):
     elements = [i for i in range(elements_no)]
     graph = {element: list() for element in elements}
     for element in elements:
-        graph[element] = list()
         number_of_connections = random.randint(0, (elements_no-1))
         for _ in range(number_of_connections):
             new_con_no = random.randint(0, (elements_no-1))
-            if elements[new_con_no] not in graph[element] and new_con_no != element:
+            if new_con_no not in graph[element] and new_con_no != element:
                 graph[element].append(elements[new_con_no])
                 if directed == True:
-                    if element not in graph[new_con_no]:
-                        graph[new_con_no].append(element)
+                    #if element not in graph[new_con_no]:
+                    graph[new_con_no].append(element)
     return graph
 
 while True:
-    graph = graph_maker(elements_no=4)
+    graph = graph_maker(elements_no=8, directed=False)
     result = bfs(graph, searched_element=2, starting_point=3)
     if result == False:
         print(graph)
